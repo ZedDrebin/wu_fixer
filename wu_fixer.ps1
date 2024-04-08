9808a92adbf97e0c1f2886f141760a34a655167c
Zed <zed@councilofnine.org>
2024-04-08 11:57:05 -0400

    Fix start bug. Add Prompt-ContinueOrQuit function at the begining.

--- wu_fixer.ps1
+++ wu_fixer.ps1
@@ -13,32 +13,36 @@ function Prompt-ContinueOrQuit {
     switch ($key.VirtualKeyCode) {
         # Space bar
         0x20 { Write-Host "Skipping..." -ForegroundColor Cyan }
-        # 'Q' key
+        # 'q' key
         0x51 { Write-Host "Quitting the script..." -ForegroundColor Red; exit }
         # Enter
         0x0D { }
         default { Write-Host "Invalid key. Please press Enter, Space bar, or 'q'." -ForegroundColor Red; Prompt-ContinueOrQuit $message }
     }
 }
-clear
+
+clear # Clear screen
+# Begin
+Prompt-ContinueOrQuit "This Script Must be run in an elevated (admin) PowerShell script. Press 'q' if you are not an admin. It will run the Windows Update Troubleshoter. Rename the C:\Windows\SoftwareDistribution folder. Run a system scan, then Run the Deployment Image Servicing and Management Tool (DISM)."
+
 # Run the Windows Update troubleshooter
-Prompt-ContinueOrQuit "Windows Update Troubleshooter"
+Prompt-ContinueOrQuit "Run Windows Update Troubleshooter?"
 msdt.exe /id WindowsUpdateDiagnostic
 
 
 # Stopping services
-Prompt-ContinueOrQuit "Windows Update Services"
+Prompt-ContinueOrQuit "Stop Windows Update Services?"
 Stop-Service -Name wuauserv -Force
 Stop-Service -Name cryptSvc -Force
 Stop-Service -Name bits -Force
 Stop-Service -Name msiserver -Force
-Prompt-ContinueOrQuit "Rename SoftwareDistribution Directory"
 
 # Renaming the SoftwareDistribution folder
+Prompt-ContinueOrQuit "Rename SoftwareDistribution Directory?"
 Rename-Item -Path C:\Windows\SoftwareDistribution -NewName SoftwareDistribution.old
-Prompt-ContinueOrQuit "Re-start Services"
 
 # Starting services
+Prompt-ContinueOrQuit "Re-start Windows Update Services?"
 Start-Service -Name wuauserv
 Start-Service -Name cryptSvc
 Start-Service -Name bits
